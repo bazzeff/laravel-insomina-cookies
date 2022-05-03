237f8825deba3d73050caf97caf580dce1802d4f
“bazzeff” <bazzeffanu@gmail.com>
2022-05-03 09:56:56 -0400

    translate controller update

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -3,29 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Google\Cloud\Translate\V2\TranslateClient;
-use Stichoza\GoogleTranslate\GoogleTranslate;
+use Google\Cloud\Translate\V2\TranslateClient;
+//use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class TranslationController extends Controller
 {
     
-    public $tr;
+    public $translate;
 
     public $success = false;
 
     public function __construct($api_key = null)
     {
-        $this->handle = new GoogleTranslate();
+        $this->handle = new TranslateClient();
     }
 
     public function index()
     {
          
-        $this->handle->setSource('en'); // Translate from English
-        $this->handle->setSource(); // Detect language automatically
-        $this->handle->setTarget('fr'); // Translate to french
-        $this->success = true;   
-        return $this->handle->setSource('en')->setTarget('fr','en')->translate('Goodbye');  
+        $translate = new TranslateClient([
+            'key' => 'api_key'
+        ]);
+        
+        // Translate text from english to french.
+        $result = $translate->translate('Hello world!', [
+            'target' => 'fr'
+        ]);
+        
+        echo $result['text'] . "\n";
+        
+        // Detect the language of a string.
+        $result = $translate->detectLanguage('Greetings from Michigan!');
+        
+        echo $result['languageCode'] . "\n";
+        
+        // Get the languages supported for translation specifically for your target language.
+        $languages = $translate->localizedLanguages([
+            'target' => 'en'
+        ]);
+        
+        foreach ($languages as $language) {
+            echo $language['name'] . "\n";
+            echo $language['code'] . "\n";
+        }
+        
+        // Get all languages supported for translation.
+        $languages = $translate->languages();
+        
+        foreach ($languages as $language) {
+            echo $language . "\n";
+        }
+        return view('components.translate', compact('languages'));
 
     }
 
